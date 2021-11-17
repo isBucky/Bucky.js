f7fe7e539f7a560473f792cada5cb1ae2e08418d
B4CKY0FC <matheusfreddo61@gmail.com>
2021-11-17 19:37:17 -0300

    New commit

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 'use strict';
 
-module.exports = {
-  version: require('./package.json').version,
-  
-  // Versions:
-  capitalizeFirstLetter: require('./src/CapitalizeFirstLetter.js'),
-  findArrayDuplicates: require('./src/FindArrayDuplicates.js'),
-  removeDupleChars: require('./src/RemoveDupleChars.js'),
-  formatSizeUnits: require('./src/FormatSizeUnits.js'),
-  compareStrings: require('./src/CompareStrings.js'),
-  similarString: require('./src/SimilarString.js'),
-  formatNumber: require('./src/FormatNumber.js'),
-  removeAcents: require('./src/RemoveAcents.js'),
-  timeFormat: require('./src/TimeFormat.js'),
-  daysAgo: require('./src/DaysAgo.js'),
-  shorten: require('./src/Shorten.js'),
-  sleep: require('./src/Sleep.js'),
-  ms: require('./src/Ms.js'),
-  
-  // Generators:
-  generatePassword: require('./src/generators/GeneratePassword.js'),
-  generateDecimal: require('./src/generators/GenerateDecimal.js'),
-  generateHex: require('./src/generators/GenerateHex.js'),
-  generateRGB: require('./src/generators/GenerateRGB.js'),
-  
-  // Verifications:
-  isEmptyDirectory: require('./src/verifications/IsEmptyDirectory.js'),
-  isDirectory: require('./src/verifications/IsDirectory.js'),
-  isEmptyFile: require('./src/verifications/IsEmptyFile.js'),
-  isEmail: require('./src/verifications/IsEmail.js'),
-  isFile: require('./src/verifications/IsFile.js'),
-  isDate: require('./src/verifications/IsDate.js'),
-  isHex: require('./src/verifications/IsHex.js'),
-  isURL: require('./src/verifications/IsURL.js'),
-  
-  // Encryptions:
-  encoder: require('./src/encryptions/Encoder.js'),
-  decoder: require('./src/encryptions/Decoder.js'),
-  compare: require('./src/encryptions/Compare.js')
-};
+exports.version = require('./package.json').version;
+
+exports.capitalizeFirstLetter = require('./src/CapitalizeFirstLetter.js');
+exports.findArrayDuplicates = require('./src/FindArrayDuplicates.js');
+exports.removeDupleChars = require('./src/RemoveDupleChars.js');
+exports.formatSizeUnits = require('./src/FormatSizeUnits.js');
+exports.compareStrings = require('./src/CompareStrings.js');
+exports.similarString = require('./src/SimilarString.js');
+exports.formatNumber = require('./src/FormatNumber.js');
+exports.removeAcents = require('./src/RemoveAcents.js');
+exports.timeFormat = require('./src/TimeFormat.js');
+exports.daysAgo = require('./src/DaysAgo.js');
+exports.shorten = require('./src/Shorten.js');
+exports.sleep = require('./src/Sleep.js');
+exports.ms = require('./src/Ms.js');
+
+exports.generatePassword = require('./src/generators/GeneratePassword.js');
+exports.generateDecimal = require('./src/generators/GenerateDecimal.js');
+exports.generateHex = require('./src/generators/GenerateHex.js');
+exports.generateRGB = require('./src/generators/GenerateRGB.js');
+
+exports.isEmptyDirectory = require('./src/verifications/IsEmptyDirectory.js');
+exports.isDirectory = require('./src/verifications/IsDirectory.js');
+exports.isEmptyFile = require('./src/verifications/IsEmptyFile.js');
+exports.isEmail = require('./src/verifications/IsEmail.js');
+exports.isFile = require('./src/verifications/IsFile.js');
+exports.isDate = require('./src/verifications/IsDate.js');
+exports.isHex = require('./src/verifications/IsHex.js');
+exports.isURL = require('./src/verifications/IsURL.js');
+
+exports.encoder = require('./src/encryptions/Encoder.js');
+exports.decoder = require('./src/encryptions/Decoder.js');
+exports.compare = require('./src/encryptions/Compare.js');
